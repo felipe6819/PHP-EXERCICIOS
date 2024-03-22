f9c2d19f627fadc5777955e3d28a4f3699d63320
felipe6819 <felipedasilvalima6819@gmail.com>
2024-03-22 14:47:25 -0300

    primeiro commit

---
+++ ex000/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da Maldicao</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "Olá Mundo!🫶✈️";
+
+        ?>
+
+    </h1>
+    <p>Vamos tentar nos livrar da maldição</p>
+
+</body>
+
+</html>
---
+++ ex002/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Exemplo PHP</title>
+</head>
+
+<body>
+    <h1>Exeemplo PHP</h1>
+    <? $nome = "felipe";
+    echo "<p>o meu nome é $nome</p>";
+
+    ?>
+
+</body>
+
+</html>
