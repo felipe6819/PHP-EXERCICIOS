898bd7415813d34b719e7ac7a086272e9905f1d8
felipe6819 <felipedasilvalima6819@gmail.com>
2024-03-27 21:55:23 -0300

    href

--- formularioHtml/cad.php
+++ formularioHtml/cad.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 
 </head>
 
@@ -17,10 +18,10 @@
         <?php
         $n = $_GET["nome"];
         $s = $_GET["sobrenome"];
-        echo " <p> é um prazer te conhecer, $n $s! Este é o meu site!";
+        echo " <p> é um prazer te conhecer,<strong>$n $s</strong>! Este é o meu site!";
 
         ?>
-    </main>
+        <a href="javascript:history.go(-1)">voltar a pagina anterior</a>
 
 </body>
 
